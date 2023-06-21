2bf2fed56a663c7cde7f9f4ff9770349c2d5844c
Duy <duynguyenn1711@gmail.com>
2023-06-21 13:16:17 +0700

    commit task 6115-ProductList

---
+++ TrainingBE/Model/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingBE.Model
+{
+    public class Discount
+    {
+        public int Percentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Discount()
+        {
+
+        }
+    }
+}
---
+++ TrainingBE/Model/ProductInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingBE.Model
+{
+    public class ProductInfo
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public double OriginalPrice { set; get; }
+        public double PriceWithDiscount { set; get; }
+        public Category Category { set; get; }
+        public Discount Discount { set; get; }
+
+    }
+}
