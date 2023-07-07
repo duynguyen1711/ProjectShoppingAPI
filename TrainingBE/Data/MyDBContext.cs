a23de2db3cb953b02897cd7fc80109c6f22489fd
Duy <duynguyenn1711@gmail.com>
2023-07-07 09:07:42 +0700

    6278_CodeFirstEF

---
+++ TrainingBE/Data/MyDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingBE.Model;
+namespace TrainingBE.Data
+{
+    public class MyDBContext: DbContext 
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options):base(options) { 
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; } 
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Product_Discount> Products_Discount { get; set;}
+    
+    }
+}
--- TrainingBE/Model/Product_Discount.cs
+++ TrainingBE/Model/Product_Discount.cs
@@ -2,6 +2,7 @@
 {
     public class Product_Discount
     {
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public int DiscountId { get; set; }
     }
